8a3c3681c4cc030692de80427af64d49c603eeba
Arnav Podichetty <arnav.p2024@gmail.com>
2025-02-13 04:13:54 -0800

    Initial Commit

---
+++ Claculator/ClaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaculatorApp.swift
+//  Claculator
+//
+//  Created by Arnav Podichetty on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ClaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
